67e56c18618d5f47f264451f72f7d4368c0f2acf
Kirill Smirnov <loha8088@gmail.com>
2022-11-04 20:02:53 +0300

    переделал структуру из kotlin на java

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
+//	`kotlin-dsl`
+	java
 	id("org.springframework.boot") version "2.5.1"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.5.21"
-	kotlin("plugin.spring") version "1.5.21"
+//	kotlin("jvm") version "1.5.21"
+//	kotlin("plugin.spring") version "1.5.21"
 //	kotlin("plugin.jpa") version "1.5.21"
 	/*OpenApi Generator*/
 	id("org.openapi.generator") version "5.4.0"
@@ -17,16 +17,16 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 /*OpenApi Generator*/
 openApiGenerate {
-	generatorName.set("kotlin-spring")
+	generatorName.set("java")
 //	httpUserAgent.set("restTemplate")
 	inputSpec.set("$projectDir/src/main/resources/openapi/mk/openapi.yml")
 	outputDir.set("$rootDir")
 	apiPackage.set("ru.k2.swaggerdemotinkoff.api")
 //	invokerPackage.set("ru.k2.swaggerdemotinkoff.invoker")
 	modelPackage.set("ru.k2.swaggerdemotinkoff.model")
-	groupId.set("ru.k2")
-	id.set("swagger-tinkoff")
-	version.set("0.0.10")
+//	groupId.set("ru.k2")
+//	id.set("swagger-tinkoff")
+//	version.set("0.0.10")
 	configOptions.put("dateLibrary", "java8")
 	configOptions.put("delegatePattern", "true")
 //	configOptions.put("gradleBuildFile", "true")
@@ -40,9 +40,9 @@ repositories {
 dependencies {
 //	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+//	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+//	implementation("org.jetbrains.kotlin:kotlin-reflect")
+//	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	/*OpenApi Generator*/
 	implementation("org.openapitools:openapi-generator-gradle-plugin:5.4.0"){
 		exclude(group = "org.slf4j", module = "slf4j-simple") // org.slf4j:slf4j-simple
@@ -52,33 +52,31 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+tasks {
+	withType<JavaCompile> {
+		options.encoding = "UTF-8"
+	}
+	withType<Test> {
+		useJUnitPlatform()
 	}
 }
 
-tasks.openApiGenerate {
-	doLast{
-
-//		/* переносит сгенерированные фалы из java подкаталога в подкаталог kotlin */
-//		copy {
-//			from("$rootDir/src/main/java/ru/k2/swaggerdemotinkoff")
-//			into("$rootDir/src/main/kotlin/ru/k2/swaggerdemotinkoff")
-//		}
-
-		/* удаляет лишние папки, которые были сгенерированы */
-		delete(
-				"$rootDir/.openapi-generator",
-				"$rootDir/src/main/kotlin/org",
-//				"$rootDir/src/main/java/org"
-//				,
-//				"$rootDir/src/main/java"
-		)
-	}
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
+//tasks.openApiGenerate {
+//	doLast{
+//
+////		/* переносит сгенерированные фалы из java подкаталога в подкаталог kotlin */
+////		copy {
+////			from("$rootDir/src/main/java/ru/k2/swaggerdemotinkoff")
+////			into("$rootDir/src/main/kotlin/ru/k2/swaggerdemotinkoff")
+////		}
+//
+//		/* удаляет лишние папки, которые были сгенерированы */
+//		delete(
+//				"$rootDir/.openapi-generator",
+//				"$rootDir/src/main/kotlin/org",
+////				"$rootDi0r/src/main/java/org"
+////				,
+////				"$rootDir/src/main/java"
+//		)
+//	}
+//}
---
+++ src/test/java/ru/k2/swaggerdemotinkoff/ApplicationTest.java
@@ -0,0 +1,12 @@
+package ru.k2.swaggerdemotinkoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
